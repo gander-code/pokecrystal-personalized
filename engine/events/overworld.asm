faabe46be50ecc98ed5820de9b89c5e4bc46085b
Michael Gosling <mgosling94@gmail.com>
2021-05-14 19:50:18 -0300

    Add new PocketPCFunction to handle using the PC

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -1619,6 +1619,29 @@ UnusedNothingHereText: ; unreferenced
 	text_far _UnusedNothingHereText
 	text_end
 
+PocketPCFunction:
+	call .LoadPocketPC
+	and $7f
+	ld [wFieldMoveSucceeded], a
+	ret
+
+.LoadPocketPC:
+	ld a, [wPlayerState]
+	ld hl, Script_LoadPocketPC
+	ld de, Script_LoadPocketPC_Register
+	call .CheckIfRegistered
+	call QueueScript
+	ld a, $1
+	ret
+
+.CheckIfRegistered:
+	ld a, [wUsingItemWithSelect]
+	and a
+	ret z
+	ld h, d
+	ld l, e
+	ret
+
 BikeFunction:
 	call .TryBike
 	and $7f
@@ -1704,6 +1727,18 @@ BikeFunction:
 .nope
 	scf
 	ret
+
+Script_LoadPocketPC:
+	reloadmappart
+	special UpdateTimePals
+	special PokemonCenterPC
+	reloadmappart
+	end
+
+Script_LoadPocketPC_Register:
+	special PokemonCenterPC
+	reloadmappart
+	end
 
 Script_GetOnBike:
 	reloadmappart
